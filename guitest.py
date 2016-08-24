19f29ee2efde6723620ec7216ec693b82bb80ccf
Chris Shaw <chrishaw@gmail.com>
2016-08-23 22:55:46 -0400

    A few more changes to get started
    Signed-off-by: Chris Shaw <chrishaw@gmail.com>

--- guitest.py
+++ guitest.py
@@ -1,6 +1,26 @@
 import Tkinter as tk
+from Tkinter import N, E, S, W, NE, NW, SE, SW, NSEW, EW, NS
 import ttk
 
-root = tk.Tk()
+class MainApplication(ttk.Frame):
+    def __init__(self, parent, *args, **kwargs):
+        ttk.Frame.__init__(self, parent, *args, **kwargs)
+        self.parent = parent
+        self.padding = (3,3,3,3)
+        self.rowconfigure(0,weight=1)
+        self.columnconfigure(0,weight=1)
+        self.grid(padx=5, pady=5, sticky=NSEW)
+        self.relief = "solid"
+        self.borderwidth = 5
 
-root.mainloop()
+        ttk.Label(self, text="Hello world").grid(row=0, column=0)
+
+        for child in self.winfo_children(): child.grid_configure(padx=5, pady=5)
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.title("Hello World Application")
+    root.rowconfigure(0,weight=1)
+    root.columnconfigure(0,weight=1)
+    MainApplication(root) #.pack(side="top", fill="both", expand=True)
+    root.mainloop()
